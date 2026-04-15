3f06ab8f4608a6cf5582818d13a95320613bdc94
Vien2004 <nguyenngocvien111@gmail.com>
2026-04-16 06:39:01 +0700

    Update VoucherCalculator.java

--- src/main/java/com/vshop/VoucherCalculator.java
+++ src/main/java/com/vshop/VoucherCalculator.java
@@ -2,8 +2,8 @@ package com.vshop;
 
 public class VoucherCalculator {
     public double calculateFinalPrice(double cartTotal, double shippingFee, double voucherValue) {
-        // Lỗi 11/11: Không chặn giá trị âm
+        // Lỗi 11/11 đã được vá bằng Poka-Yoke
         double finalPrice = cartTotal + shippingFee - voucherValue;
-        return finalPrice;
+        return Math.max(0, finalPrice);
     }
 }
